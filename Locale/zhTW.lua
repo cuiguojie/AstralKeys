96227181d8c52b07638e9c8bdbf8b6cc5c56897b
cuiguojie <cuiguojie@me.com>
2017-08-22 00:11:49 +0800

    use AceLocale

---
+++ Locale/zhTW.lua
@@ -0,0 +1,23 @@
+------------------------------------------------------------------------------
+-- Traditional Chinese localization --by cuiguojie
+------------------------------------------------------------------------------
+local L = LibStub("AceLocale-3.0"):NewLocale("AstralKeys", "zhTW")
+
+L['Eye of Azshara'] = '艾薩拉之眼'
+L['Darkheart Thicket'] = '暗心灌木林'
+L['Black Rook Hold'] = '玄鴉堡'
+L['Halls of Valor'] = '英靈殿'
+L['Neltharion\'s Lair'] = '耐薩里奧巢穴'
+L['Vault of the Wardens'] = '看守者鐵獄'
+L['Maw of Souls'] = '靈魂之喉'
+L['The Arcway'] = '幽暗地道'
+L['Court of Stars'] = '眾星之廷'
+L['Return to Karazhan: Lower'] = '重返卡拉讚: 下層'
+L['Cathedral of Eternal Nightr'] = '永夜聖殿'
+L['Return to Karazhan: Upper'] = '重返卡拉讚: 上層'
+
+L['Level'] = '傳奇等級'
+L['Dungeon'] = '地城'
+L['Player'] = '角色'
+
+L['Keystone'] = '鑰石'
